b7915246c7c6426b15277440f92cc59c982412bf
Brian Mock <mock.brian@gmail.com>
2012-01-25 22:47:56 -0800

    Making some progress on the axes.

--- 2/Grapher.java
+++ 2/Grapher.java
@@ -3,11 +3,18 @@ import javax.swing.*;
 
 public class Grapher extends JFrame {
     private final Canvas canvas;
+    private final Color bgColor = new Color( 17,  17,  17);
+    private final Color fgColor = new Color(224, 224, 224);
 
     public Grapher(DataList dl) {
         canvas = new Canvas(dl);
         canvas.setPreferredSize(new Dimension(800, 480));
-        add(canvas);
+        setBackground(bgColor);
+        setForeground(fgColor);
+        setLayout(new BorderLayout());
+        add(canvas, BorderLayout.CENTER);
+        add(new YAxis(dl),   BorderLayout.WEST);
+        add(new XAxis(dl), BorderLayout.SOUTH);
         pack();
         setVisible(true);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
